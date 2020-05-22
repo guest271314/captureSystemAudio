c8a33a680dd89d578604c86edc0030a9f543cb53
guest271314 <guest271314@gmail.com>
2020-05-21 21:48:05 -0700

    Update captureSystemAudio.js

--- captureSystemAudio.js
+++ captureSystemAudio.js
@@ -3,9 +3,8 @@ async function captureSystemAudio() {
     const requestNativeScript = new Map();
     requestNativeScript.set('dir', await self.chooseFileSystemEntries({type:'openDirectory'}));
     requestNativeScript.set('status', await requestNativeScript.get('dir').requestPermission({writable:true}));
-    // inotifywait does not fire events for file with no content, add space character to text file
+    // inotifywait does not fire events for getFile(), or file with no content, add space character to text file
     // FileSystemFileHandle.getFile() does not open file https://bugs.chromium.org/p/chromium/issues/detail?id=1084840
-    // inotifywait does not fire access or open events
     requestNativeScript.set('start', await (await requestNativeScript.get('dir').getFile('captureSystemAudio.txt',{create:false})).getFile());
     requestNativeScript.set('stop', await (await requestNativeScript.get('dir').getFile('stopSystemAudioCapture.txt',{create:false})).getFile());
     // Execute File.arrayBuffer() to read file for inotifywait to fire access or open event

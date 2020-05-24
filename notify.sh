69cdcb6adbdcb5ec4127c7b47b769a24326a73d7
guest271314 <guest271314@gmail.com>
2020-05-24 23:23:27 +0000

    Update notify.sh

--- notify.sh
+++ notify.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 start() {
-  inotifywait -m -e open captureSystemAudio.txt | while read file; 
+  inotifywait -m -e open $HOME/localscripts/captureSystemAudio.txt | while read file; 
   do 
-    echo "$file" | grep "captureSystemAudio.txt OPEN" && ./captureSystemAudio.sh
+    echo "$file" | grep "captureSystemAudio.txt OPEN" && $HOME/localscripts/captureSystemAudio.sh
   done
 }
 stop() {
-  inotifywait -m -e open stopSystemAudioCapture.txt | while read file;
+  inotifywait -m -e open $HOME/localscripts/stopSystemAudioCapture.txt | while read file;
   do 
-    echo "$file" | grep "stopSystemAudioCapture.txt OPEN" && ./stopSystemAudioCapture.sh
+    echo "$file" | grep "stopSystemAudioCapture.txt OPEN" && $HOME/localscripts/stopSystemAudioCapture.sh
   done
 }
 start & stop

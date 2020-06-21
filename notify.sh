2cb60b377f2edb75509c0f07c3ef3a04545d29c6
guest271314 <guest271314@gmail.com>
2020-06-21 14:56:57 +0000

    Include capability to launch pavucontrol

--- notify.sh
+++ notify.sh
@@ -1,14 +1,30 @@
 #!/bin/sh
 start() {
   inotifywait -m -e open $HOME/localscripts/captureSystemAudio.txt | while read file; 
-  do 
-    echo "$file" | grep "captureSystemAudio.txt OPEN" && $HOME/localscripts/captureSystemAudio.sh
-  done
+    do 
+      echo "$file" | grep "captureSystemAudio.txt OPEN" && $HOME/localscripts/captureSystemAudio.sh
+    done
 }
+
 stop() {
   inotifywait -m -e open $HOME/localscripts/stopSystemAudioCapture.txt | while read file;
-  do 
-    echo "$file" | grep "stopSystemAudioCapture.txt OPEN" && $HOME/localscripts/stopSystemAudioCapture.sh
-  done
+    do 
+      echo "$file" | grep "stopSystemAudioCapture.txt OPEN" && $HOME/localscripts/stopSystemAudioCapture.sh
+    done
 }
-start & stop
+
+openPavuControl() {
+  inotifywait -m -e open $HOME/localscripts/openpavucontrol.txt | while read file; 
+    do 
+      echo "$file" | grep "openpavucontrol.txt OPEN" && $HOME/localscripts/openpavucontrol.sh
+    done
+}
+
+closePavuControl() {
+  inotifywait -m -e open $HOME/localscripts/closepavucontrol.txt | while read file;
+    do 
+      echo "$file" | grep "closepavucontrol.txt OPEN" && $HOME/localscripts/closepavucontrol.sh
+    done
+}
+  
+start & stop & openPavuControl & closePavuControl

ba8e0f55512cc53b89a67b213220a0a5214aa90c
guest271314 <guest271314@gmail.com>
2021-06-28 20:55:59 -0700

    Create capture_system_audio.sh

---
+++ native_messaging/capture_system_audio/capture_system_audio.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+sendMessage() {
+  # https://stackoverflow.com/a/24777120
+  message="$1"
+  # Calculate the byte size of the string.
+  # NOTE: This assumes that byte length is identical to the string length!
+  # Do not use multibyte (unicode) characters, escape them instead, e.g.
+  # message='"Some unicode character:\u1234"'
+  messagelen=${#message}
+  # Convert to an integer in native byte order.
+  # If you see an error message in Chrome's stdout with
+  # "Native Messaging host tried sending a message that is ... bytes long.",
+  # then just swap the order, i.e. messagelen1 <-> messagelen4 and
+  # messagelen2 <-> messagelen3
+  messagelen1=$(( ($messagelen      ) & 0xFF ))               
+  messagelen2=$(( ($messagelen >>  8) & 0xFF ))               
+  messagelen3=$(( ($messagelen >> 16) & 0xFF ))               
+  messagelen4=$(( ($messagelen >> 24) & 0xFF ))               
+  # Print the message byte length followed by the actual message.
+  printf "$(printf '\\x%x\\x%x\\x%x\\x%x' \
+    $messagelen1 $messagelpen2 $messagelen3 $messagelen4)%s" "$message"
+}
+
+capture_system_audio() {
+  if pgrep -f 'php -S localhost:8000' > /dev/null; then
+    pkill -f 'php -S localhost:8000' & sendMessage '"Local server off."' 
+  else
+    php -S localhost:8000 & sendMessage '"Local server on."'
+  fi
+}
+capture_system_audio

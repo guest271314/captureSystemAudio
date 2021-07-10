772d775767b289c2f43090073bdd15278902cc99
guest271314 <guest271314@gmail.com>
2021-07-10 12:29:04 -0700

    Add AbortController; abort() fetch() before stopping PHP

--- native_messaging/capture_system_audio/transferableStream.js
+++ native_messaging/capture_system_audio/transferableStream.js
@@ -6,21 +6,29 @@ onload = async () => {
       parent.postMessage(nativeMessage, name);
       await new Promise((resolve) => setTimeout(resolve, 20));
       parent.postMessage('Ready.', name);
+      const controller = new AbortController();
+      const { signal } = controller;
+      signal.onabort = (e) => console.log(e);
       onmessage = async (e) => {
         if (e.data instanceof ReadableStream) {
-          const { value: file, done } = await e.data.getReader().read();
-          const fd = new FormData();
-          const stdin = await file.text();
-          fd.append(file.name, stdin);
-          const { body } = await fetch('http://localhost:8000', {
-            method: 'post',
-            body: fd,
-            cache: 'no-store',
-          });
-          parent.postMessage(body, name, [body]);
+          try {
+            const { value: file, done } = await e.data.getReader().read();
+            const fd = new FormData();
+            const stdin = await file.text();
+            fd.append(file.name, stdin);
+            const { body } = await fetch('http://localhost:8000', {
+              method: 'post',
+              body: fd,
+              cache: 'no-store',
+              signal,
+            });
+            parent.postMessage(body, name, [body]);
+          } catch (err) {
+            console.error(err);
+          }
         }
-
         if (e.data === 'Abort.') {
+          controller.abort();
           chrome.runtime.sendNativeMessage(
             'capture_system_audio',
             {},

473879422aa9f6a5dfe3c015547650a425724610
guest271314 <guest271314@gmail.com>
2021-09-18 23:49:56 -0700

    Update transferableStream.js

--- native_messaging/capture_system_audio/transferableStream.js
+++ native_messaging/capture_system_audio/transferableStream.js
@@ -4,7 +4,7 @@ onload = () => {
   const id = 'capture_system_audio';
   const port = chrome.runtime.connectNative(id);
   // Chromium 96: onMessage event handler dispatched after disconnect()
-  const handleMessage = ({ value, done }, p) => {
+  const handleMessage = ({ value, done }) => {
     return writer.write(new Uint8Array(base64ToBytesArr(value)))
       .catch((err) => {
         console.log(err.message, performance.now());

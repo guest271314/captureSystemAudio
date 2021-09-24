d4c7bce0280cf58f16d01289c052cea57fb8af7e
guest271314 <guest271314@gmail.com>
2021-09-24 00:01:41 -0700

    Update transferableStream.js

--- native_messaging/capture_system_audio/transferableStream.js
+++ native_messaging/capture_system_audio/transferableStream.js
@@ -1,25 +1,32 @@
-onload = () => {
+onload = async () => {
   const { readable, writable } = new TransformStream();
   const writer = writable.getWriter();
   const id = 'capture_system_audio';
   const port = chrome.runtime.connectNative(id);
-  // Chromium 96: onMessage event handler dispatched after disconnect()
-  const handleMessage = ({ value, done }) => {
-    return writer.write(new Uint8Array(base64ToBytesArr(value)))
-      .catch((err) => {
-        console.log(err.message, performance.now());
-        parent.postMessage('Done.', name);
-        port.disconnect(id);
-        onmessage = null;
-        if ('gc' in globalThis) gc();
-        return false;
-      });
+  const disconnect = async () => {
+    if (port.onMessage.hasListener(handleMessage)) {
+      port.onMessage.removeListener(handleMessage);
+      parent.postMessage('Done.', name);
+      onmessage = null;
+      await writer.abort();
+    }
+    return port.disconnect(id);
   };
-  port.onDisconnect.addListener((e) => {
+  const handleMessage = async ({ value }) => {
+    try {
+      await writer.ready;
+      await writer.write(new Uint8Array(base64ToBytesArr(value)));
+    } catch (err) {
+      console.warn(err);
+      return await disconnect();
+    }
+  };
+  await writer.ready;
+  port.onDisconnect.addListener(async (e) => {
     console.log(e);
   });
   port.onMessage.addListener(handleMessage);
-  onmessage = (e) => {
+  onmessage = async (e) => {
     const { type, message } = e.data;
     if (type === 'start') {
       port.postMessage({
@@ -28,8 +35,8 @@ onload = () => {
       parent.postMessage(readable, name, [readable]);
     }
     if (type === 'stop') {
-      console.log(type, message, port);
-      port.disconnect(id);
+      console.log(type);
+      return await disconnect();
     }
   };
   parent.postMessage('Ready.', name);

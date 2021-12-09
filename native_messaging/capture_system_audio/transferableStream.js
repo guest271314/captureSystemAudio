cbb4c5a4c123c0d4119fdc09097f3fa03d747ee0
guest271314 <guest271314@gmail.com>
2021-12-08 18:22:21 -0800

    Update transferableStream.js

--- native_messaging/capture_system_audio/transferableStream.js
+++ native_messaging/capture_system_audio/transferableStream.js
@@ -1,35 +1,20 @@
-onload = async () => {
+onload = () => {
   const { readable, writable } = new TransformStream();
   const writer = writable.getWriter();
   const id = 'capture_system_audio';
-  const port = chrome.runtime.connectNative(id);
-  const disconnect = async () => {
-    if (port.onMessage.hasListener(handleMessage)) {
-      port.onMessage.removeListener(handleMessage);
-      parent.postMessage('Done.', name);
-      onmessage = null;
-      await writer.abort().then(() => console.log('writer aborted')).catch(console.error);
-    }
-    return port.disconnect(id);
-  };
-  const handleMessage = async (value) => {
+  let port = chrome.runtime.connectNative(id);
+  let handleMessage = async (value) => {
     try {
-      await writer.ready;
-      await writer.write(new Uint8Array(JSON.parse(value)));
-    } catch (err) {
-      console.warn(err.message);
-      try {
-        if (port.onMessage.hasListener(handleMessage)) {
-          await disconnect();
-        }
-      } catch (e) {
-        console.warn(e.message);
+      if (writable.locked) {
+        await writer.ready;
+        await writer.write(new Uint8Array(JSON.parse(value)));
       }
+    } catch (e) {
+      console.warn(e.message);
     }
   };
-  await writer.ready;
   port.onDisconnect.addListener(async (e) => {
-    console.log(e);
+    console.warn(e.message);
   });
   port.onMessage.addListener(handleMessage);
   onmessage = async (e) => {
@@ -41,9 +26,20 @@ onload = async () => {
       parent.postMessage(readable, name, [readable]);
     }
     if (type === 'stop') {
-      console.log(type);
-      return await disconnect();
+      try {
+        await writer.close();
+        await writer.closed;
+        console.log('Writer closed.');
+        writer.releaseLock();
+        port.onMessage.removeListener(handleMessage);
+        port.disconnect(id);
+        port = null;
+        parent.postMessage(0, name);
+        onmessage = null;
+      } catch (err) {
+        console.warn(err.message);
+      }
     }
   };
-  parent.postMessage('Ready.', name);
+  parent.postMessage(1, name);
 };

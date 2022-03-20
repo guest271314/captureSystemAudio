543984c26f210d747d8eb843eb3429926c414c91
guest271314 <guest271314@gmail.com>
2022-03-20 08:52:42 -0700

    Remove while loop after FILE* pipe

--- native_messaging/capture_system_audio/capture_system_audio.cpp
+++ native_messaging/capture_system_audio/capture_system_audio.cpp
@@ -17,7 +17,7 @@ struct message_t {
 json get_message() {
   char raw_length[4];
   fread(raw_length, 4, sizeof(char), stdin);
-  uint32_t message_length = *reinterpret_cast<uint32_t *>(raw_length);
+  uint32_t message_length = *reinterpret_cast<uint32_t*>(raw_length);
   if (!message_length) {
     exit(EXIT_SUCCESS);
   }
@@ -38,33 +38,31 @@ message_t encode_message(json content) {
 
 // Send an encoded message to stdout.
 void send_message(message_t encoded_message) {
-  char *raw_length = reinterpret_cast<char *>(&encoded_message.length);
+  char* raw_length = reinterpret_cast<char*>(&encoded_message.length);
   fwrite(raw_length, 4, sizeof(char), stdout);
   fwrite(encoded_message.content.c_str(), encoded_message.length, sizeof(char),
          stdout);
   fflush(stdout);
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
   while (true) {
     json message = get_message();
     auto command = message.get<std::string>();
     // https://github.com/paullouisageneau/libdatachannel/issues/544#issuecomment-1024739214
     // https://www.reddit.com/r/cpp_questions/comments/swr1tk/comment/hxral9f/
-    FILE *pipe = popen(command.c_str(), "r");
-    while (true) {
-      size_t bufferSize = 1024;
-      byte buffer[bufferSize];
-      size_t count;
-      while ((count = fread(buffer, 1, bufferSize, pipe)) > 0) {
-        // S16NE PCM from parec to integers in JSON array
-        json data = json::array();
-        for (size_t i = 0; i < count; i++) {
-          data.push_back((int)buffer[i]);
-        }
-        // send JSON array to Native Messaging local client
-        send_message(encode_message(data));
+    FILE* pipe = popen(command.c_str(), "r");
+    size_t bufferSize = 1024;
+    byte buffer[bufferSize];
+    size_t count;
+    while ((count = fread(buffer, 1, bufferSize, pipe)) > 0) {
+      // S16NE PCM from parec to integers in JSON array
+      json data = json::array();
+      for (size_t i = 0; i < count; i++) {
+        data.push_back((int)buffer[i]);
       }
+      // send JSON array to Native Messaging local client
+      send_message(encode_message(data));
     }
   }
 }

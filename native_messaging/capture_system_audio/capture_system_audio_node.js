6850002e73f1b9ee991bafaf9ff6dd64f6b48a3a
guest271314 <guest271314@gmail.com>
2022-07-03 12:26:54 -0700

    Pass flags to consume less memory

--- native_messaging/capture_system_audio/capture_system_audio_node.js
+++ native_messaging/capture_system_audio/capture_system_audio_node.js
@@ -1,9 +1,10 @@
-#!node
+#!/usr/bin/env -S /path/to/node --max-old-space-size=6 --expose-gc --v8-pool-size=1 --jitless
 // Node.js Native Messaging host
 // https://github.com/simov/native-messaging/blob/master/protocol.js
 // https://github.com/simov/native-messaging/blob/master/nodejs/example.js
 // Might be good to use an explicit path to node on the shebang line
 // in case it isn't in PATH when launched by Chrome
+process.env.UV_THREAD_POOL = 1;
 process.stdin.on('readable', () => {
   let input = [];
   let chunk;
@@ -11,21 +12,24 @@ process.stdin.on('readable', () => {
     input.push(chunk);
   }
   input = Buffer.concat(input);
-  const msgLen = input.readUInt32LE(0);
-  const dataLen = msgLen + 4;
+  let msgLen = input.readUInt32LE(0);
+  let dataLen = msgLen + 4;
   if (input.length >= dataLen) {
-    const content = input.slice(4, dataLen);
-    const json = JSON.parse(content.toString());
-    handleMessage(json);
+    let content = input.slice(4, dataLen);
+    handleMessage(JSON.parse(content.toString()));
+    input = chunk = msgLen = dataLen = null;
+    global.gc();
   }
 });
 
 function sendMessage(msg) {
-  const buffer = Buffer.from(JSON.stringify(msg));
-  const header = Buffer.alloc(4);
+  let buffer = Buffer.from(JSON.stringify(msg));
+  let header = Buffer.alloc(4);
   header.writeUInt32LE(buffer.length, 0);
-  const data = Buffer.concat([header, buffer]);
+  let data = Buffer.concat([header, buffer]);
   process.stdout.write(data);
+  msg = buffer = header = data = null;
+  global.gc();
 }
 
 process.on('uncaughtException', (err) => {
@@ -33,16 +37,17 @@ process.on('uncaughtException', (err) => {
 });
 
 function handleMessage(input) {
-  const [command, ...args] = input.split(' ');
+  let [command, ...args] = input.split(' '),
+    chunk;
   let { spawn } = require('child_process');
   let child = spawn(command, args);
-  child.stdout.on('data', (data) => {
-    sendMessage([...new Uint8Array(data.buffer, data.byteOffset, data.byteLength / Uint8Array.BYTES_PER_ELEMENT)]);
-  });
-  child.stderr.on('data', (data) => {
-    // sendMessage(data);
-  });
-  child.on('close', (code) => {
-   // sendMessage('closed');
-  });
+  function read() {
+    while (null !== (chunk = child.stdout.read())) {
+      sendMessage([...chunk]);
+      chunk = null;
+      global.gc();
+    }
+  }
+  child.stdout.on('readable', read);
+  input = command = args = null;
 }
